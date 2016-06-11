7b3dfe23175c42770bee28e0753323d60ddf9ddc
diegolanu89 <diegolanus89@gmail.com>
2016-06-11 16:06:03 -0300

    Delete EstadoHumanoide.java

--- src/fiuba/algo3/modelo/EstadoHumanoide.java
+++
@@ -1,9 +0,0 @@
-package fiuba.algo3.modelo;
-/**
- * Created by Mauro on 08/06/2016.
- */
-public class EstadoHumanoide extends Estado {
-	public void aplicarEfectoDelCasillero(Casillero posicion) {
-		posicion.efectoTerrestre(this);
-	}
-}
