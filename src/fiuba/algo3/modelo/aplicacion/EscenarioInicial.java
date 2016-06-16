5d4dbff38f9b07ad7bb638aec95a0ef0c84de02c
martinvedia <martin.vedia@hotmail.com>
2016-06-16 15:35:39 -0300

    Se corrije error en EscenarioInicial.java

--- src/fiuba/algo3/modelo/aplicacion/EscenarioInicial.java
+++ src/fiuba/algo3/modelo/aplicacion/EscenarioInicial.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
-;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
