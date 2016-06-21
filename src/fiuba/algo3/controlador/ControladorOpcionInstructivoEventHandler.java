d726c6e85f0e73a8e8a5375535faae21a8d20d53
mauro <mauro_baldinelli34@hotmail.com>
2016-06-20 21:45:32 -0300

    quito ñ de un comentario q hizo q no pase la prueba

--- src/fiuba/algo3/controlador/ControladorOpcionInstructivoEventHandler.java
+++ src/fiuba/algo3/controlador/ControladorOpcionInstructivoEventHandler.java
@@ -15,7 +15,7 @@ public class ControladorOpcionInstructivoEventHandler implements EventHandler<Ac
         String mensaje = "Convencion para la superficie segun el color de los casilleros\n"
         		+ "Color Griz: Superficie Roca no afecta en ningun estado\n"
         		+ "Color Verde Oscuro: Superficie pantano quita un %50 en su velocidad en estado terrestre\n"
-        		+ "Color Verde claro: Superficie espinas 5% de daño en cualquier estado\n"
+        		+ "Color Verde claro: Superficie espinas 5% de danio en cualquier estado\n"
         		+ "Color Celeste: Superficie Nube no afecta en ningun estado\n"
         		+ "Color Violeta oscuro: Superficie Tormenta Psionica reduce %40 el ataque en estado aereo\n"
         		+ "Color Violeta claro: Superficie Nebulosa de Andromeda bloquea 3 turnos en estado aereo\n"
