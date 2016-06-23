240fc75df4e0ea515c142f955275cf82b4ed3300
martinvedia <martin.vedia@hotmail.com>
2016-06-23 15:56:21 -0300

    Se quita la excepcion SobrepasaDistanciaDeAtaqueException por desuso

--- src/fiuba/algo3/modelo/SobrepasaDistanciaDeAtaqueException.java
+++
@@ -1,9 +0,0 @@
-package fiuba.algo3.modelo;
-
-/**
- * Created by Vietnamita on 31/05/2016.
- */
-public class SobrepasaDistanciaDeAtaqueException extends RuntimeException {
-
-    public SobrepasaDistanciaDeAtaqueException(){super();}
-}
